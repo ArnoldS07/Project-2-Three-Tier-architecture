492856dce290943957e8a943a6d2b4fd07c48514
Arnold <arnoldstar77@gmail.com>
2025-08-21 08:55:58 +0530

    Update main.tf

--- Terraform/modules/rds/main.tf
+++ Terraform/modules/rds/main.tf
@@ -6,13 +6,20 @@ resource "aws_db_subnet_group" "this" {
 resource "aws_security_group" "db" {
   name   = "${var.name}-db-sg"
   vpc_id = var.vpc_id
+
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
     security_groups = [var.db_sg_source_id]
   }
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_db_instance" "this" {
@@ -30,4 +37,7 @@ resource "aws_db_instance" "this" {
   skip_final_snapshot    = true
   multi_az               = true
 }
-output "endpoint" { value = aws_db_instance.this.address }
+
+output "endpoint" {
+  value = aws_db_instance.this.address
+}
