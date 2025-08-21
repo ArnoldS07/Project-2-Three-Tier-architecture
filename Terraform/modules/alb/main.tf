6b19c8b338228f10605ed36283e2b1d54d6de8cc
Arnold <arnoldstar77@gmail.com>
2025-08-21 08:49:25 +0530

    Update main.tf

--- Terraform/modules/alb/main.tf
+++ Terraform/modules/alb/main.tf
@@ -10,15 +10,28 @@ resource "aws_lb_target_group" "app" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-  health_check { path = "/" matcher = "200-399" }
+
+  health_check {
+    path     = "/"
+    matcher  = "200-399"
+  }
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
-  default_action { type = "forward" target_group_arn = aws_lb_target_group.app.arn }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
 }
 
-output "dns_name" { value = aws_lb.this.dns_name }
-output "tg_arn"   { value = aws_lb_target_group.app.arn }
+output "dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "tg_arn" {
+  value = aws_lb_target_group.app.arn
+}
