80be4056c656888f03f750e22e805bc9e115cb49
Arnold <arnoldstar77@gmail.com>
2025-08-21 09:41:02 +0530

    Update variables.tf

--- Terraform/modules/vpc/variables.tf
+++ Terraform/modules/vpc/variables.tf
@@ -1,3 +1,6 @@
 variable "name"    { type = string }
 variable "vpc_cidr"{ type = string }
-variable "azs"     { type = list(string) }
+variable "azs" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b"]
+}
