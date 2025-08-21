15de8e34d057ae30dd31e312c8801c967befc240
Arnold <arnoldstar77@gmail.com>
2025-08-21 09:38:08 +0530

    Update variables.tf

--- Terraform/modules/ec2_asg/variables.tf
+++ Terraform/modules/ec2_asg/variables.tf
@@ -1,14 +1,55 @@
-variable "name"                { type = string }
-variable "region"              { type = string }
-variable "private_app_subnets" { type = list(string) }
-variable "app_sg_id"           { type = string }
-variable "alb_tg_arn"          { type = string }
-variable "instance_type"       { type = string }
-variable "desired_capacity"    { type = number }
-variable "iam_instance_profile"{ type = string }
-variable "ecr_repo_frontend"   { type = string }
-variable "ecr_repo_backend"    { type = string }
-variable "db_host"             { type = string }
-variable "db_user"             { type = string }
-variable "db_pass"             { type = string }
-variable "db_port"             { type = number }
+variable "name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "private_app_subnets" {
+  type = list(string)
+}
+
+variable "app_sg_id" {
+  type = string
+}
+
+variable "alb_tg_arn" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "desired_capacity" {
+  type = number
+}
+
+variable "iam_instance_profile" {
+  type = string
+}
+
+variable "ecr_repo_frontend" {
+  type = string
+}
+
+variable "ecr_repo_backend" {
+  type = string
+}
+
+variable "db_host" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_pass" {
+  type = string
+}
+
+variable "db_port" {
+  type = number
+}
