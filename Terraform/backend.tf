155c3bf91cf1d1f42729877dcd89789a61a912c3
Arnold <arnoldstar77@gmail.com>
2025-08-21 10:29:41 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-proj-terra-state"
+    bucket         = "my-proj-terra-state" 
     key            = "three-tier/dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "my-terra-state-lock"
