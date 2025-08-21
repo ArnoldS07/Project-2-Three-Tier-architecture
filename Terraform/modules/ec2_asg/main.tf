cc8fb66b148b92ae63eafcdacfe1c259a59fb7b4
Arnold <arnoldstar77@gmail.com>
2025-08-21 13:17:54 +0530

    Update main.tf

--- Terraform/modules/ec2_asg/main.tf
+++ Terraform/modules/ec2_asg/main.tf
@@ -4,49 +4,42 @@ data "aws_ssm_parameter" "al2023_ami" {
 
 resource "aws_launch_template" "this" {
   name_prefix             = "${var.name}-lt-"
-  image_id                = data.aws_ssm_parameter.al2023_ami.value
+  image_id                = data.aws_ssm_parameter.ubuntu_ami.value
   instance_type           = var.instance_type
   vpc_security_group_ids  = [var.app_sg_id]
-
-  # Add your key pair here
-  key_name = var.key_name   # pass the key name via variable
+  key_name                = var.key_name
 
   iam_instance_profile {
     name = var.iam_instance_profile
   }
-}
-  user_data = base64encode(
-    templatefile("${path.module}/user_data.sh.tftpl", {
-      region            = var.region
-      ecr_repo_frontend = var.ecr_repo_frontend
-      ecr_repo_backend  = var.ecr_repo_backend
-      db_host           = var.db_host
-      db_user           = var.db_user
-      db_pass           = var.db_pass
-      db_port           = var.db_port
-    })
-  )
 
+  # User data must be base64 encoded if using aws_launch_template
+  user_data = base64encode(templatefile("${path.module}/user_data.sh.tftpl", {
+    region            = var.region
+    ecr_repo_frontend = var.ecr_repo_frontend
+    ecr_repo_backend  = var.ecr_repo_backend
+    db_host           = var.db_host
+    db_user           = var.db_user
+    db_pass           = var.db_pass
+    db_port           = var.db_port
+  }))
+
+  # Tags go inside launch template like this:
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "${var.name}-app"
+      Name = "${var.name}-instance"
     }
   }
 }
 
 resource "aws_autoscaling_group" "this" {
-  name                = "${var.name}-asg"
-  desired_capacity    = var.desired_capacity
-  max_size            = var.desired_capacity
-  min_size            = var.desired_capacity
-  vpc_zone_identifier = var.private_app_subnets
-
+  desired_capacity     = var.desired_capacity
+  max_size             = var.desired_capacity
+  min_size             = var.desired_capacity
+  vpc_zone_identifier  = var.private_app_subnets
   launch_template {
     id      = aws_launch_template.this.id
     version = "$Latest"
   }
-
-  target_group_arns = [var.alb_tg_arn]
-  health_check_type = "EC2"
 }
